7de741c3e8130088f814933bd031c2e70e69089c
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2023-07-18 20:04:59 +0200

    Log time measure

--- partyplanner/src/main/java/org/partyplanner/Main.java
+++ partyplanner/src/main/java/org/partyplanner/Main.java
@@ -1,17 +1,23 @@
 package org.partyplanner;
 
 import java.io.IOException;
+import java.time.Instant;
 import java.time.MonthDay;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 
+import static java.time.temporal.ChronoUnit.MILLIS;
+
 public class Main {
     public static void main(String[] args) throws IOException {
-        List<Person> people = new PersonReader().readAllPersons("employees1000.csv");
+        Instant startOfThisApplication = Instant.now();
+        String inputFile = "employees1000.csv";
+        List<Person> people = new PersonReader().readAllPersons(inputFile);
         printPersons(people);
         MonthDay biggestParty = findBiggestParty(people);
         String formattedBiggestParty = biggestParty.format(DateTimeFormatter.ofPattern("dd.MM"));
         System.out.println("Biggest party: " + formattedBiggestParty);
+        System.out.println("Time spent: " + MILLIS.between(startOfThisApplication, Instant.now()) + " ms for input file " + inputFile);
     }
 
     private static MonthDay findBiggestParty(List<Person> people) {
